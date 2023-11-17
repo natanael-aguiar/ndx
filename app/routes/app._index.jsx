9f1e08d7f0e16a6d459e06e3175878dcead6bcb5
Enrique Souza <enriquesouza@live.com>
2023-11-17 14:30:47 -0300

    ndx

---
+++ app/routes/app._index.jsx
@@ -0,0 +1,59 @@
+import { useEffect } from "react";
+import { json } from "@remix-run/node";
+import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
+import {
+  Page,
+  Layout,
+  Text,
+  Card,
+  Button,
+  BlockStack,
+  Box,
+  List,
+  Link,
+  InlineStack,
+} from "@shopify/polaris";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+
+  return null;
+};
+
+export const action = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+
+  return json({
+    webhook: null,
+  });
+};
+
+export default function Index() {
+  const nav = useNavigation();
+  const actionData = useActionData();
+  const submit = useSubmit();
+  const isLoading =
+    ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
+  const productId = actionData?.product?.id.replace(
+    "gid://shopify/Product/",
+    ""
+  );
+
+  useEffect(() => {
+    if (productId) {
+      shopify.toast.show("Product created");
+    }
+  }, [productId]);
+  const generateProduct = () => submit({}, { replace: true, method: "POST" });
+
+  return (
+    <Page>
+      <ui-title-bar title="Setup NDX Application">
+        <button variant="primary" onClick={generateProduct}>
+          Setup NDX
+        </button>
+      </ui-title-bar>
+    </Page>
+  );
+}
